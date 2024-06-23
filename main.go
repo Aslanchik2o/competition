a5729362b45ae954d67c3a948ecb7cd43bf4e9cd
Aslanchik2o <108986320+Aslanchik2o@users.noreply.github.com>
2024-06-23 12:37:13 +0500

    gorutin

--- main.go
+++ main.go
@@ -1,59 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+var b int
 
-type Number interface{
-	int64 | float64
-}
-type User struct{
-	email string
-	name string
-}
+func main() {
+	go func(){
+	time.Sleep(time.Millisecond)
+	fmt.Println("competitive with delay")
+}()
+	
 
-func main(){
-	a := []int64{1,313,4}
-	b := []float64{2.313,1424.235,325}
-	c := [] string{"1","2","32"}
-	d := []User{
-		{
-			email: "steal@gmail.com",
-			name: "Durka",
-		},
-		{
-			email: "thin@gmail.com",
-			name: "Cat",
-		},
-		{	email: "thin1@gmail.com",
-		name: "Cat2",
-	},
-	}
-	fmt.Println(sum(a))
-	fmt.Println(sum(b))
-	fmt.Println(c)
-	fmt.Println(searchElement(d, User{
-		email: "thin1@gmail.com",
-		name: "Cat2",
-	}))
-	printAny(d)
-}
-
-func sum[V int64 | float64] (input []V)V {
-	var result V
-	for _, number := range input {
-		result += number
-	}
-	return result
-}
-func searchElement[C comparable](elements []C, SearchEl C) bool {
-	for _, el := range elements{
-		if el == SearchEl {
-return true
-		}
-	}
-	return false
-}
-func printAny[A any] (input A){
-	fmt.Println(input)
-
+	go fmt.Println("competitive")
+	go fmt.Println("competitive")
+	time.Sleep(2 * time.Second)
 }
